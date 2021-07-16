c555f0650f74266807fc420f532a5676a9b11bd2
rekha <yrekha0820@gmail.com>
2021-07-16 12:48:26 -0700

    Adding first python code, listOverLap

--- LIstOverLap.py
+++ LIstOverLap.py
@@ -19,7 +19,11 @@ def listOverLap(a,b):
     for x in range(len(a)):
         for y in range(len(b)):
             if a[x] == b[y]:
-                c.append(a[x])
+                if a[x] not in c :
+                    c.append(a[x])
     return c
 
-print("Common elements between 2 list",listOverLap([1,2],[1,2,5,7]))
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+print("Common elements between 2 list",listOverLap(a,b))
